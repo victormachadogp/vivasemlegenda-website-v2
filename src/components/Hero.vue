f6256097f341c0b11e9f24b47921f9fc38b8d609
Victor Machado <victormachadogp@gmail.com>
2019-12-18 18:24:40 -0300

    Revert "Deleted hello world Vue file"
    This reverts commit 2962e7cfb09af19b9651b482f2bfc45b4280ceb3.

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,12 +1,6 @@
 <template>
     <div class="hero">
-        <section class="container">
-            <img class="image-cover" src="assets/path-hero.svg">
-            <div class="grid">
-                <img class="logo-page" src="assets/logo-original.svg">
-                <h1>A MELHOR ESCOLHA PARA AS MELHORES PESSOAS!</h1>
-            </div>
-        </section>
+        Esse é o hero
     </div>
 </template>
 
@@ -17,44 +11,9 @@
 </script>
 
 <style scoped>
-/* .hero{
+.hero{
     background: rosybrown;
     width: 100%;
     height: 300px;
-} */
-
-.container {
-    width: 100%;
-    position: relative;
-    text-align: center;
-}
-
-.image-cover {
-    width: 100%;
-    pointer-events: none;
-}
-.grid {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    position: absolute;
-    top: 37%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    font-weight: 100;
-    width: 100%;
-}
-h1 {
-    align-self: center;
-    justify-self: center;
-    width: 70%;
-    color: white;
-    font-size: 3vw;
-    font-weight: 100;
-}
-
-.logo-page {
-    width: 60%;
-    align-self: center;
-    justify-self: center;
 }
 </style>

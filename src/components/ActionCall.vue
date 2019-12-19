f5e314222dbb0bd5792be635d017a6c66c9a8f83
Victor Machado <victormachadogp@gmail.com>
2019-12-18 22:29:14 -0300

    Call to Action Section Added

---
+++ src/components/ActionCall.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="grid-container">
+        <div class="text">
+            <h4>COMECE HOJE!</h4> 
+            <p>De acordo com site de empregos Catho, apenas 5%
+            da população brasileira fala uma segunda língua e
+            menos de 3% têm fluência em inglês. Isso significa
+            começar com vantagem quando se trata do mercado </p>
+        </div>
+
+        <div>
+            <img class="desk-man" src="assets/desktop-man.svg">
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+.grid-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+}
+
+.text {
+    justify-self: center;
+    align-self: center;
+}
+
+.desk-man {
+    width: 100%;
+}
+
+</style>

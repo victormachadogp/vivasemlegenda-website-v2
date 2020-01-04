422c37be002b8967227039f88afa8ae880066407
Victor Machado <victormachadogp@gmail.com>
2020-01-04 00:29:36 -0300

    flags added to the banner

--- src/components/BannerPrincipal.vue
+++ src/components/BannerPrincipal.vue
@@ -1,9 +1,41 @@
 <template>
   <div class="section-grid">
-    <p>Quer entender por que a Viva Sem Legenda é a melhor escolha para as melhores pessoas? Agende sua aula experimental e comece a viver sem legenda já!</p>
+      <p class="subtitle">Quer entender por que a Viva Sem Legenda é a melhor escolha para as melhores pessoas? Agende sua aula experimental e comece a viver sem legenda já!</p>
   <div class="flags">
-    <p>Flags
-    </p>
+    <div class="us flag-container">
+      <img class="country-flag" src="assets/us-flag.svg">
+      <p class="country-title">Inglês</p>
+    </div>
+
+    <div class="fr flag-container">
+      <img class="country-flag" src="assets/fr-flag.svg">
+      <p class="country-title">Francês</p>
+    </div>
+
+    <div class="sp flag-container">
+      <img class="country-flag" src="assets/sp-flag.svg">
+      <p class="country-title">Espanhol</p>
+    </div>
+
+    <div class="sk flag-container">
+      <img class="country-flag" src="assets/sk-flag.svg">
+      <p class="country-title">Coreano</p>
+    </div>
+
+    <div class="ch flag-container">
+      <img class="country-flag" src="assets/ch-flag.svg">
+      <p class="country-title">Chinês</p>
+    </div>
+
+    <div class="sa flag-container">
+      <img class="country-flag" src="assets/sa-flag.svg">
+      <p class="country-title">Árabe</p>
+    </div>
+
+    <div class="br flag-container">
+      <img class="country-flag" src="assets/br-flag.svg">
+      <p class="country-title">Português</p>
+    </div>
 
   </div>
 
@@ -24,6 +56,7 @@ export default {};
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-template-rows: 20%;
+  width: 100%;
 }
 
 .image-container {
@@ -37,4 +70,62 @@ export default {};
   width: 35%;
 }
 
+.country-flag {
+  width: 70px;
+}
+
+.flags {
+  display: flex;
+  flex-wrap: wrap;
+  padding-left: 2%;
+  margin: 2%;
+  justify-content: center;
+}
+
+.flag-container {
+  padding-left: 3%;
+  padding-right: 3%;
+}
+
+.subtitle {
+  font-size: 1.2rem;
+  padding-left: 3%;
+}
+
+.country-title {
+  font-size: 1.1em;
+  padding-top: 10%;
+}
+
+@media (max-width: 900px) {
+
+  .section-grid {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: auto;
+  width: 100%;
+}
+
+.subtitle {
+  margin-left: 2%;
+  margin-right: 2%;
+  margin-bottom: 5%;
+}
+
+.flags {
+  margin-bottom: 5%;
+
+}
+
+.flag-container {
+  padding: 2%;
+}
+
+.image-container {
+    display: none;
+
+  }
+
+
+}
 </style>

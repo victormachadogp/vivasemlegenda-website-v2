f85f6d2e7d9378e8c1d0ff31ce143a1a58f43357
Jonathan Machado <jonathan@Laurens-MacBook-Air.local>
2020-01-05 16:00:05 +0000

    Design adjustments to the CursosPage.vue

--- src/views/CursosPage.vue
+++ src/views/CursosPage.vue
@@ -2,54 +2,41 @@
   <div>
     <TheNavBar />
     <!-- <div class="content">content here</div> -->
-      <div class="grid">
-        <h1>Cursos</h1>
+    <div class="grid">
+      <h1>Cursos</h1>
       <div>
         <img class="image-top" src="assets/desktop-man.svg" />
       </div>
-      </div>
-      <div class="background">
-        <div class="cursos-box">
-          <div class="flex">
+    </div>
+    <div class="background">
+      <div class="cursos-box">
+        <div class="flex">
           <h2>Aulas Online</h2>
-            <p>
-              Por meio da plataforma Zoom você pode aprender Inglês, Espanhol, Francês, Mandarim, Coreano, Árabe e Português para estrangeiros. Após o cadastro gratuito na plataforma Zoom com o nosso auxilio, é só agendar sua próxima aula e mãos à obra.
-            </p>
-          <p>
-            Na plataforma Zoom o aluno terá acesso em áudio e vídeo ao professor no horário marcado para a sua aula, além disso o aluno terá acesso aos materiais preparados para a aula como livros, slides e áudios. Você terá uma sala de aula com a exclusividade e atenção que merece a um click de distância.
-          </p>
-          </div>
-          <img class="image-box" src="assets/man-computer.svg" />
+          <p>Por meio da plataforma Zoom você pode aprender Inglês, Espanhol, Francês, Mandarim, Coreano, Árabe e Português para estrangeiros. Após o cadastro gratuito na plataforma Zoom com o nosso auxilio, é só agendar sua próxima aula e mãos à obra.</p>
+          <p>Na plataforma Zoom o aluno terá acesso em áudio e vídeo ao professor no horário marcado para a sua aula, além disso o aluno terá acesso aos materiais preparados para a aula como livros, slides e áudios. Você terá uma sala de aula com a exclusividade e atenção que merece a um click de distância.</p>
         </div>
-
-        <div class="cursos-box middle-box">
-          <img class="image-box" src="assets/deal.svg" />
-          <div class="flex">
-          <h2>Aulas para Empresas</h2>
-            <p>
-              A Viva Sem Legenda tem como objetivo ajudar os colaboradores da sua empresa a obter um segundo idioma como parte do seu desenvolvimento profissional visando uma alta qualidade profissional para sua empresa. Essas aulas podem acontecer on-line ou dentro da sua própria empresa para grupos de até seis pessoas.
-            </p>
-          </div>
-        </div>
-
-        <div class="cursos-box">
-          <div class="flex">
-          <h2>Aulas On Demand</h2>
-            <p>
-              Quer uma aula apenas para tirar aquela dúvida sobre um assunto específico?
-            </p>
-          <p>
-          Quer praticar para aquela apresentação profissional com o representante estrangeiro da sua empresa?
-          </p>
-
-          <p>
-          Então você precisa conhecer a aula on-demand da Viva Sem Legenda. Nesse método você contrata uma aula no dia e horário que você desejar para tratar do assunto que você quiser. Estude on-demand e Viva Sem Legenda.
-          </p>
-          </div>
-          <img class="image-box" src="assets/contact-us.svg" />
-        </div>
-
+        <img class="image-box" src="assets/desktop-man.svg" />
       </div>
+
+      <div class="cursos-box middle-box">
+        <img class="image-box" src="assets/deal.svg" />
+        <div class="flex">
+          <h2>Aulas para Empresas</h2>
+          <p>A Viva Sem Legenda tem como objetivo ajudar os colaboradores da sua empresa a obter um segundo idioma como parte do seu desenvolvimento profissional visando uma alta qualidade profissional para sua empresa. Essas aulas podem acontecer on-line ou dentro da sua própria empresa para grupos de até seis pessoas.</p>
+        </div>
+      </div>
+
+      <div class="cursos-box">
+        <div class="flex">
+          <h2>Aulas On Demand</h2>
+          <p>Quer uma aula apenas para tirar aquela dúvida sobre um assunto específico?</p>
+          <p>Quer praticar para aquela apresentação profissional com o representante estrangeiro da sua empresa?</p>
+
+          <p>Então você precisa conhecer a aula on-demand da Viva Sem Legenda. Nesse método você contrata uma aula no dia e horário que você desejar para tratar do assunto que você quiser. Estude on-demand e Viva Sem Legenda.</p>
+        </div>
+        <img class="image-box" src="assets/contact-us.svg" />
+      </div>
+    </div>
     <TheFooter />
   </div>
 </template>
@@ -95,32 +82,33 @@ h1 {
     padding-top: 3%;
     padding-bottom: 3%;
     z-index: -1;
-    margin-bottom: 5%;
 }
 
 .cursos-box {
   background-color: #fff;
   border-radius: 10px;
-  margin: 3%;
+  margin: 3% 6%;
   display: grid;
-  grid-template-columns: 80% 20%;
+  grid-template-columns: 2fr 1fr;
   padding: 2.5%;
+  column-gap: 2rem;
 }
 
-// .flex {
-//   width: 80%;
-// }
+.flex{
+  display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
 
 .image-box {
-  width: 80%;
+  width: 100%;
   align-self: center;
   justify-self: center;
 }
 
 .middle-box {
   display: grid;
-  grid-template-columns: 20% 80%;
-  
+  grid-template-columns: 1fr 2fr;
 }
 
 h2 {

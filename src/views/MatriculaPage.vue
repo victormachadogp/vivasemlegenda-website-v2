a2a0f27c7dba0538b20937a7fb56cf1dd1c9a205
Victor Machado <victormachadogp@gmail.com>
2020-01-06 03:34:05 -0300

    Matricula page with content

--- src/views/MatriculaPage.vue
+++ src/views/MatriculaPage.vue
@@ -9,102 +9,120 @@
         <img class="image-top" src="assets/chat.svg" />
       </div>
     </div>
+  
 
+<div class="container-form">
+  <div class="inside-form">
 
-<form>
+<form class="form form-grid gform" method="POST" action="https://script.google.com/macros/s/AKfycbyWCSvvDraBDa2gM4vwy7cnh3ariXXSSGwgbD3tw9mD3cxetecN/exec">
 
     <!-- <div> -->
-      <div class="background">
-      <div class="container-form">
       <div class="form-row">
-        <div class="form-group">
-            <label for="inputPassword4">Nome</label>
-            <input type="text" class="form-control" id="inputPassword4" placeholder="Nome">
+        <div class="form-group col-md-6">
+            <label for="name">Nome</label>
+            <input id="name" name="name" type="text" class="form-control" placeholder="Nome">
         </div>
-        <div class="form-group">
-            <label for="inputEmail4">Email</label>
-            <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+        <div class="form-group col-md-6">
+            <label for="email">Email</label>
+            <input id="email" name="email" type="email" class="form-control" placeholder="Email">
         </div>
+    </div>
 
-        <div class="form-group-2">
-          <label for="inputAddress">Endereço</label>
-          <input type="text" class="form-control" id="inputAddress" placeholder="1234 Rua das Flores">
-        </div> 
+    <div class="form-group">
+        <label for="inputAddress">Endereço</label>
+        <input id="endereco" name="endereco" type="text" class="form-control" placeholder="1234 Rua das Flores">
     </div>
 
     <div class="form-row">
       <div class="form-group col-md-6">
           <label for="inputPassword4">RG</label>
-          <input type="number" class="form-control" id="inputPassword4" placeholder="000000000">
+          <input id="rg" name="rg" type="number" class="form-control" placeholder="000000000">
       </div>
       <div class="form-group col-md-6">
           <label for="inputEmail4">CPF</label>
-          <input type="number" class="form-control" id="inputEmail4" placeholder="00000000000">
+          <input id="cpf" name="cpf" type="number" class="form-control" placeholder="00000000000">
       </div>
-    </div>
+  </div>
 
-    <div class="form-row">
-        <div class="form-group col-md-6">
-            <label for="inputPassword4">Telefone Fixo</label>
-            <input type="number" class="form-control" id="inputPassword4" placeholder="(00)0000-0000">
-        </div>
-        <div class="form-group col-md-6">
-            <label for="inputEmail4">Celular</label>
-            <input type="number" class="form-control" id="inputEmail4" placeholder="(00)00000-0000">
-        </div>
-    </div>
+  <div class="form-row">
+      <div class="form-group col-md-6">
+          <label for="inputPassword4">Telefone Fixo</label>
+          <input id="telfix" name="telfix" type="number" class="form-control" placeholder="(00)0000-0000">
+      </div>
+      <div class="form-group col-md-6">
+          <label for="inputEmail4">Celular</label>
+          <input id="telcel" name="telcel" type="number" class="form-control" placeholder="(00)00000-0000">
+      </div>
+  </div>
 
-    <div class="form-row">
-      <div class="form-group col-md-12">
-        <label for="inputState">Selecione a língua desejada</label>
-        <select id="inputState" class="form-control">
-        <option selected>Inglês</option>
-        <option selected>Espanhol</option>
-        <option selected>Francês</option>
-        <option selected>Coreano</option>
-        <option selected>Chinês</option>
-        <option selected>Árabe</option>
-        <option selected>Português (Para Estrangeiros)</option>
-        <option selected>Escolher...</option>
-        </select>
+   <div class="form-row" id="cloneForm">
+      <div class="form-group col-md-6" >
+              <label for="inputState">Selecione o dia das aulas:</label>
+              <select name="dia" class="form-control">
+              <option selected>Segunda-Feira</option>
+              <option selected>Terça-Feira</option>
+              <option selected>Quarta-Feira</option>
+              <option selected>Quinta-Feira</option>
+              <option selected>Sexta-Feira</option>
+              <option selected>Escolher...</option>
+              </select>
+          </div>			 
+          <div class="form-group col-md-6" >
+              <label for="inputState">Selecione o Período das aulas:</label>
+              <select name="dia" class="form-control">
+              <option selected>Manhã</option>
+              <option selected>Tarde</option>
+              <option selected>Noite</option>
+              <option selected>Escolher...</option>
+              </select>
+          </div>			 
+  </div>			 
+   <!-- Form 2 -->
+<div class="form-row" >
+  <div class="form-group col-md-6">
+          <label for="inputState">Selecione o dia das aulas:</label>
+          <select name="dia" class="form-control">
+          <option selected>Segunda-Feira</option>
+          <option selected>Terça-Feira</option>
+          <option selected>Quarta-Feira</option>
+          <option selected>Quinta-Feira</option>
+          <option selected>Sexta-Feira</option>
+          <option selected>Escolher...</option>
+          </select>
       </div>			 
-      </div>
-
-      <div class="form-row">
-    <div class="form-group col-md-6">
-            <label for="inputState">Selecione o dia das aulas:</label>
-            <select id="inputState" class="form-control">
-            <option selected>Segunda-Feira</option>
-            <option selected>Terça-Feira</option>
-            <option selected>Quarta-Feira</option>
-            <option selected>Quinta-Feira</option>
-            <option selected>Sexta-Feira</option>
-            <option selected>Escolher...</option>
-            </select>
-        </div>			 
-<div class="form-group col-md-6 text-center">
-    <label for="inputState">Selecione o período das aulas:</label>
-    <div class="form-radios"></div>
-        <div class="form-check form-check-inline">
-            <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
-            <label class="form-check-label" for="inlineCheckbox1">Manhã</label>
-        </div>
-        <div class="form-check form-check-inline">
-            <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
-            <label class="form-check-label" for="inlineCheckbox2">Tarde</label>
-        </div>
-        <div class="form-check form-check-inline">
-            <input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3">
-            <label class="form-check-label" for="inlineCheckbox3">Noite</label>
-        </div>
-    </div>
-</div>			
-
-<button type="submit" class="btn btn-primary btn-lg">Enviar</button>
+      <div class="form-group col-md-6">
+          <label for="inputState">Selecione o Período das aulas:</label>
+          <select name="dia" class="form-control">
+          <option selected>Manhâ</option>
+          <option selected>Tarde</option>
+          <option selected>Noite</option>
+          <option selected>Escolher...</option>
+          </select>
+      </div>			
 </div>
+<p class="morethen" id="clickHere">Adicione mais de 2 aulas por semana</p>
+
+<!-- Field Against Bots -->
+<fieldset class="pure-group honeypot-field">
+    <label for="honeypot">To help avoid spam, utilize a Honeypot technique with a hidden text field; must be empty to submit the form! Otherwise, we assume the user is a spam bot.</label>
+    <input id="honeypot" type="text" name="honeypot" value="" />
+  </fieldset>
+
+
+<button type="submit" class="button-success pure-button button-xlarge btn btn-primary btn-lg">&nbsp;Enviar</button>
+
+<!-- Thank You Message -->
+<div class="thankyou_message" style="display:none;">
+    <h2>Obrigado por nos contatar!
+      Vamos analisar o seu pedido e responder o mais rapido possivel.</h2>
 </div>
+
+
+     
 
 </form>
+</div>
+</div>
 
 
     
@@ -127,23 +145,6 @@ export default {
 <style lang="scss" scoped>
 .content {
   height: 600px;
-}
-
-.form-row {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-    justify-items: start;
-
-}
-
-.form-group-2 {
-  grid-column-start: 1;
-  grid-column-end: 3;
-  margin: 2%;
-}
-
-.form-group {
-  margin: 2%;
 }
 
 .grid {
@@ -170,12 +171,66 @@ h1 {
   padding: 5%;
 }
 
-.container-form {
-  margin: 3%;
-  background-color: #fff;
-}
-
 .label {
   font-size: 1.1rem;
 }
+
+// New Code
+.form-grid {
+	background-color: white;
+	margin: 5%;
+	padding: 5%;
+	border-radius: 20px;
+}
+
+.form-radios {
+	margin: 2%;
+	padding-left: 10% !important;
+}
+
+/* Form Code */
+
+.morethen {
+
+	color: rgb(74, 74, 218);
+	cursor: pointer;
+}
+
+.honeypot-field {
+	display: none;
+  }
+
+.container-form {
+  background-image: linear-gradient(to right, #0095f5, #0095f5, #50bafe);
+  padding-top: 5%;
+  padding-bottom: 5%;
+  margin-bottom: 2%;
+  }
+
+.form-group {
+  text-align: left;
+  }
+
+.morethen {
+  text-align: left;
+}
+
+@media (max-width: 600px) {
+
+  .grid {
+  grid-template-columns: 1fr;
+  justify-items: center;
+}
+
+  h1 {
+    margin: 5%;
+    text-align: center !important;
+
+  }
+
+  .image-top {
+    display: none;
+  }
+}
+
 </style>

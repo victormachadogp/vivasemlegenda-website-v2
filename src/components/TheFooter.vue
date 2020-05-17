471f23249699b4782fc7d5ac2bc08c349522051e
Victor Machado <victormachadogp@gmail.com>
2020-05-17 12:46:26 -0300

    Number phone changed

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -14,7 +14,7 @@
       <p>contato@vivasemlegenda.com.br</p>
 
       <svg id="whatsapp-logo" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20 22.621l-3.521-6.795c-.008.004-1.974.97-2.064 1.011-2.24 1.086-6.799-7.82-4.609-8.994l2.083-1.026-3.493-6.817-2.106 1.039c-7.202 3.755 4.233 25.982 11.6 22.615.121-.055 2.102-1.029 2.11-1.033z"/></svg>
-      <a class="link-whatsapp" href="https://api.whatsapp.com/send?phone=5511981719466">(11) 98171-9466</a>
+      <a class="link-whatsapp" href="https://api.whatsapp.com/send?phone=5513997928404">(13) 99792-8404</a>
     </div>
     <!-- <div class="developed-by">Developed by xxxx</div> -->
   </div>
